923bb64af8ca5ad5cd1cdb508dd023b426c9c80f
kboygit <kirbyy.james@gmail.com>
2017-03-15 07:22:39 +0000

    built my about page,update development.rb to read changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'quotes#index'
+  root  'quotes#index'
+  get   'about', to: 'quotes#about'
   resources :quotes
 end
